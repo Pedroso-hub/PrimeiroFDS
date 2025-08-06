a12eee0c9f36c7f291137edcc5456a88ce8b5b41
Pedroso-hub <66489432+Pedroso-hub@users.noreply.github.com>
2025-08-06 01:04:22 +0000

    second

--- projetoJava/src/App.java
+++ projetoJava/src/App.java
@@ -1,5 +1,15 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public static void main(String[] args) {
+    System.out.println("\nHello world!");
+    String version = System.getProperty("java.version");
+    System.out.println("Running Java Version: "+version+"\n");
+    String name = System.getProperty("user.name");
+    System.out.println("User account name: "+name+"\n");
+    String currDir = System.getProperty("user.dir");
+    String versaoOs = System.getProperty("os.version");
+    String dirHome = System.getProperty("user.home");
+    System.out.println("diretorio atual do usuário: "+currDir);
+    System.out.println("diretorio home do usuário: "+dirHome);
+    System.out.println("versão do OS: "+versaoOs);
     }
 }
